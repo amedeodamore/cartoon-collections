ba696c35c4e0517bb9221b4a1c032fac3dfea54a
Amedeo d'Amore <amedeodamore@gmail.com>
2020-11-22 15:51:50 +0000

    commit tree ee218507d32f24e446de500b430642d8f3ec2f66

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves_list)
+    new_list = []
+    while count < dwarves_list.length
+    dwarves_list.each_with_index do  {|dwarves, index|}
+    new_list << "#{index}. #{dwarves}"
+    end
+    puts new_list
 end
 
-def summon_captain_planet# code an argument here
+#def summon_captain_planet# code an argument here
   # Your code here
-end
+#end
 
-def long_planeteer_calls# code an argument here
+#def long_planeteer_calls# code an argument here
   # Your code here
-end
+#end
 
-def find_the_cheese# code an argument here
+#def find_the_cheese# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  #cheese_types = ["cheddar", "gouda", "camembert"]
+#end
